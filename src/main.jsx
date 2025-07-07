ff9ca0641f9d845ed69a4271aff9ce53522bd276
Camiloruas <miloruas@gmail.com>
2025-07-06 21:27:22 -0300

    Criação da Router

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,22 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.jsx";
-import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Taskpages from './pages/taskpage.jsx';
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <App/>,
+    path: '/',
+    element: <App />,
+  },
+  {
+    path: '/task',
+    element: <Taskpages/>,
   },
 ]);
-createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router}/>
-
+    <RouterProvider router={router} />
   </StrictMode>
 );
