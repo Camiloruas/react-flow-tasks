f1a0df43c20e92eae83d0f8243e51af7c9a10043
Camiloruas <miloruas@gmail.com>
2025-07-08 16:31:34 -0300

    correção de caminho da main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import Taskpages from './pages/taskpage.jsx';
+import Taskpages from './pages/Taskpage.jsx';
 
 const router = createBrowserRouter([
   {
