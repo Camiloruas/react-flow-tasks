3e0e6337db5f374e01daed5a101937ce52cca48c
Camiloruas <miloruas@gmail.com>
2025-07-08 17:07:53 -0300

    Correcao do import Taskpage na main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import Taskpages from './pages/taskpage.jsx';
+import Taskpage from './pages/Taskpage.jsx'; // SEM 's' no final de Taskpage no import
 
 const router = createBrowserRouter([
   {
@@ -12,7 +12,7 @@ const router = createBrowserRouter([
   },
   {
     path: '/task',
-    element: <Taskpages/>,
+    element: <Taskpage />,
   },
 ]);
 createRoot(document.getElementById('root')).render(
