30830d6b18c6a1a2c2e085ada7fd3e5a3d92e9df
Camiloruas <miloruas@gmail.com>
2025-07-08 11:27:30 -0300

    acrescentado funcionalidade de Edição da tarefa

---
+++ src/components/EditTask.jsx
@@ -0,0 +1,59 @@
+
+import { useState } from 'react';
+import PropTypes from 'prop-types';
+
+function EditTask({ task, onTaskEdit, onCancel }) {
+  const [title, setTitle] = useState(task.title);
+  const [description, setDescription] = useState(task.description);
+
+  return (
+    <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center">
+      <div className="bg-white p-6 rounded-md shadow-lg w-1/3">
+        <h2 className="text-2xl font-bold mb-4">Editar Tarefa</h2>
+        <div className="space-y-4">
+          <input
+            type="text"
+            placeholder="Digite o título da tarefa"
+            className="p-2 border border-slate-300 outline-slate-400 px-4 py-2 rounded-md w-full"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+          <input
+            type="text"
+            placeholder="Digite a descrição da tarefa"
+            className="p-2 border border-slate-300 outline-slate-400 px-4 py-2 rounded-md w-full"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+          />
+        </div>
+        <div className="flex justify-end space-x-4 mt-6">
+          <button
+            onClick={onCancel}
+            className="bg-slate-400 text-white px-4 py-2 rounded-md"
+          >
+            Cancelar
+          </button>
+          <button
+            onClick={() => {
+              if (!title.trim() || !description.trim()) {
+                return alert('Por favor, preencha o título e a descrição da tarefa.');
+              }
+              onTaskEdit(task.id, title, description);
+            }}
+            className="bg-slate-500 text-white px-4 py-2 rounded-md"
+          >
+            Salvar
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default EditTask;
+
+EditTask.propTypes = {
+  task: PropTypes.object.isRequired,
+  onTaskEdit: PropTypes.func.isRequired,
+  onCancel: PropTypes.func.isRequired,
+};
