17ad4966364b5d9ac9529a71102fabb0a0218915
Camiloruas <miloruas@gmail.com>
2025-07-10 21:24:46 -0300

    Ajustado  responsividade da pág de edição

--- src/components/EditTask.jsx
+++ src/components/EditTask.jsx
@@ -8,7 +8,7 @@ function EditTask({ task, onTaskEdit, onCancel }) {
 
   return (
     <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center">
-      <div className="bg-white p-6 rounded-md shadow-lg w-1/3">
+      <div className="bg-white p-6 rounded-md shadow-lg w-11/12 md:w-1/3">
         <h2 className="text-2xl font-bold mb-4">Editar Tarefa</h2>
         <div className="space-y-4">
           <input
@@ -26,10 +26,10 @@ function EditTask({ task, onTaskEdit, onCancel }) {
             onChange={(e) => setDescription(e.target.value)}
           />
         </div>
-        <div className="flex justify-end space-x-4 mt-6">
+        <div className="flex flex-col md:flex-row justify-end md:space-x-4 mt-6">
           <button
             onClick={onCancel}
-            className="bg-slate-400 text-white px-4 py-2 rounded-md"
+            className="bg-slate-400 text-white px-4 py-2 rounded-md mt-2 md:mt-0"
           >
             Cancelar
           </button>
@@ -40,7 +40,7 @@ function EditTask({ task, onTaskEdit, onCancel }) {
               }
               onTaskEdit(task.id, title, description);
             }}
-            className="bg-slate-500 text-white px-4 py-2 rounded-md"
+            className="bg-slate-500 text-white px-4 py-2 rounded-md mt-2 md:mt-0"
           >
             Salvar
           </button>
